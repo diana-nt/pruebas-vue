f337250b9710c06eaceefa7cd441590247a9cbe7
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-09-25 10:35:49 +0200

    Organize files again

---
+++ JavaScript moderno/Sets/test.js
@@ -0,0 +1,19 @@
+const users = ["Javi", "Luis", "Raúl", "Rubén", "Sergio", "Jorge", "Perales", "Samuel"];
+const userVisits = new Set();
+
+function visit() {
+    const userIndex = Math.floor(Math.random() * users.length);
+    const user = users[userIndex];
+    userVisits.has(user) &&
+    console.warn(`User ${user} already visited the page!`);
+    userVisits.add(user);
+    console.log(userVisits);
+
+    if (userVisits.size === users.length) {
+        console.info("All users have visited the page!");
+        clearInterval(interval);
+        userVisits.clear();
+    }
+}
+
+const interval = setInterval(visit, 300);
