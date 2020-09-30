c5a6745c789bcdc46d9c6f748a7c92c949a98da5
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-09-30 09:58:24 +0200

    Save unversioned files init app

---
+++ Vue intermedio/Single File Components/css-en-sfc/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Tareas></Tareas>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Tareas from '@/components/Tareas'
+
+export default {
+  name: 'Home',
+  components: {
+    Tareas
+  }
+}
+</script>
