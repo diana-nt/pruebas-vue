8d90b3b105e53b84aa873d18030f74f45e072c31
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-09-30 10:27:38 +0200

    Register base components automatically

--- Vue intermedio/Single File Components/autoimportar-componentes/src/App.vue
+++ Vue intermedio/Single File Components/autoimportar-componentes/src/App.vue
@@ -4,9 +4,19 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+<!--    <BaseForm/>-->
     <router-view/>
   </div>
 </template>
+
+<script>
+  // import BaseForm from "@/components/BaseForm";
+  export default {
+    components: {
+      // BaseForm
+    }
+  }
+</script>
 
 <!--<style> lang="scss"-->
 <!--/*@import url('assets/main.css');*/-->
