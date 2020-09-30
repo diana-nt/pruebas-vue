eaa78494b6deebbd9aa243b642c846dcb1212196
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-09-30 09:54:19 +0200

    Use of CSS in SFC

---
+++ Vue intermedio/Single File Components/logicasfc/src/components/Tarea.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>
+      {{ titulo }}
+      <button @click="$emit('eliminarTarea')">Eliminar</button>
+    </h3>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: "Tarea.vue",
+    props: ['titulo']
+  }
+</script>
+
+<style scoped>
+
+</style>
