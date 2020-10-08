4d812e86dc8edc619d1eb7ed9b5cd74c004f79f4
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-10-08 10:32:27 +0200

    Add vuex introduction

---
+++ vuex/src/components/AppProductList.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <div>
+        <h1>Listado de productos</h1>
+        <hr />
+        <ul>
+            <li v-for="product in products" :key="product.id">
+                {{ product.title }} | {{ product.price }}
+                <i>{{ product.inventory }} </i>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+// import api from "../api/shop.js";
+export default {
+    name: "AppProductList",
+    created() {
+        // api.getProducts(products => {
+        //   this.products = products;
+        // });
+    },
+    computed: {
+        products() {
+            return this.$store.state.products;
+        }
+    }
+};
+</script>
+
+<style scoped>
+ul {
+    text-align: left;
+}
+</style>
