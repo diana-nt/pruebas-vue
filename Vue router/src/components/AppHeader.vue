6b5059967f65fddd35f8c7bc029a1ec291141c78
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-11-10 10:03:41 +0100

    Add Vue Router lessons

---
+++ Vue router/src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="header">
+    <h1 class="header__title">User Directory</h1>
+    <nav class="header__nav">
+      <RouterLink name="Directory" dest="/" />
+      <RouterLink name="Contact" dest="/contact" />
+      <RouterLink name="Legal" dest="/legal" />
+    </nav>
+  </header>
+</template>
+
+<script>
+import RouterLink from "./RouterLink";
+export default {
+  name: "AppHeader",
+  components: {
+    RouterLink
+  }
+};
+</script>
+
+<style scoped>
+.header {
+  text-align: center;
+  padding-bottom: 1rem;
+  position: fixed;
+  width: 100%;
+  z-index: 1;
+  background-color: rgba(0, 0, 0, 0.9);
+}
+
+.header__title {
+  color: goldenrod;
+}
+
+.header__nav {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
