914848d0e1a212496adbcd8929fba240bc6336ea
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-11-10 10:29:00 +0100

    Add vue router unversioned files

---
+++ Vue router/vue-router/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <AppHeader />
+    <RouterView />
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader";
+import RouterView from "./components/RouterView";
+export default {
+  name: "App",
+  components: {
+    RouterView,
+    AppHeader
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: sans-serif;
+  background-color: lightgray;
+}
+</style>
